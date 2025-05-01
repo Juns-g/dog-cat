f9182f988f168a7d744d5c02a93b9899f8a6a759
Juns <juns.g@foxmail.com>
2025-05-01 17:12:05 +0800

    refactor: 简化CatDogClassifier组件，移除Tabs，直接渲染ImageClassifier；清理app.css，删除不必要的样式

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,17 +1,8 @@
 import ImageClassifier from "./components/ImageClassifier";
-import { Tabs } from "antd";
-
-const tabItems = [
-  {
-    key: "1",
-    label: "单张图片分类",
-    children: <ImageClassifier />,
-  },
-];
 
 const CatDogClassifier = () => (
   <div className="cat-dog-classifier">
-    <Tabs defaultActiveKey="1" items={tabItems} />
+    <ImageClassifier />
   </div>
 );
 
